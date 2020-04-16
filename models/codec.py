12340edbba7c6fc68d63f2600f299704d4990400
sunnythree <1318288270@qq.com>
2020-04-16 23:57:44 +0800

    add framwork files

---
+++ models/codec.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class CodecNet(nn.Module):
+    def __init__(self):
+        super(CodecNet, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
