8dcf42a640c1e96860eb3f12a7b86b40ba102fc2
sunnythree <1318288270@qq.com>
2020-04-22 20:31:17 +0800

    test get test dataset

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from torchvision import transforms as tfs
 MODEL_SAVE_PATH = "./data/codec_seg.pt"
 
 def test():
-    data_loader = DataLoader(dataset=SegDataSet(160, 160, True), batch_size=1, shuffle=True, num_workers=1)
+    data_loader = DataLoader(dataset=SegDataSet(160, 160, False), batch_size=1, shuffle=True, num_workers=1)
     use_cuda = torch.cuda.is_available()
     #device = torch.device("cuda" if use_cuda else "cpu")
     device = torch.device("cpu")
